3829aa698ded96d0af4cbae731d082d7c6635175
Sangram Keshari Ghose <sangramkesharighose@gmail.com>
2026-08-01 00:59:20 +0530

    Add service worker for caching resources

---
+++ frontend/service-worker.js
@@ -0,0 +1,9 @@
+const CACHE = 'deploytual-v3';
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
